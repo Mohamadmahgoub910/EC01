72db331c29c3bc539af0812365eb6a5a353734ae
Mohamadmahgoub910 <mohamadmahgoub1@Outlook.com>
2025-10-04 19:08:32 +0300

    Applying Filters

--- EC01/Controllers/HomeController.cs
+++ EC01/Controllers/HomeController.cs
@@ -16,10 +16,58 @@ namespace EC01.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(FilterProductVM filterProductVM)
         {
+            const decimal discount = 50;
             var products = _context.Products/*.AsNoTracking()*/.Include(e=>e.Category).AsQueryable();
             // Add Filter 
+            if (filterProductVM.name is not null)
+            {
+                products = products.Where(e => e.Name.Contains(filterProductVM.name));
+                ViewBag.name = filterProductVM.name;
+            }
+
+            if (filterProductVM.minPrice is not null)
+            {
+                products = products.Where(e => e.Price - e.Price * e.Discount / 100 > filterProductVM.minPrice);
+                ViewBag.minPrice = filterProductVM.minPrice;
+            }
+
+            if (filterProductVM.maxPrice is not null)
+            {
+                products = products.Where(e => e.Price - e.Price * e.Discount / 100 < filterProductVM.maxPrice);
+                ViewBag.maxPrice = filterProductVM.maxPrice;
+            }
+
+            if (filterProductVM.categoryId is not null)
+            {
+                products = products.Where(e => e.CategoryId == filterProductVM.categoryId);
+                ViewBag.categoryId = filterProductVM.categoryId;
+            }
+
+            if (filterProductVM.brandId is not null)
+            {
+                products = products.Where(e => e.BrandId == filterProductVM.brandId);
+                ViewBag.brandId = filterProductVM.brandId;
+            }
+
+            if (filterProductVM.isHot)
+            {
+                products = products.Where(e => e.Discount > discount);
+                ViewBag.isHot = filterProductVM.isHot;
+            }
+
+            // Categories
+            var categories = _context.Categories;
+            //ViewData["categories"] = categories.AsEnumerable();
+            ViewBag.categories = categories.AsEnumerable();
+
+            // Brands
+            var brands = _context.Brands;
+            ViewData["brands"] = brands.AsEnumerable();
+
+
+
             return View(products.ToList());
         }
 
---
+++ EC01/Models/FilterProductVM.cs
@@ -0,0 +1,6 @@
+﻿namespace EC01.Models
+{
+    public record FilterProductVM (string name, decimal? minPrice, decimal? maxPrice, int? categoryId, int? brandId, bool isHot)
+    {
+    }
+}
