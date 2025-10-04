3dc11faa0ee8fba697739d30e9d9b104a38e2bc2
Mohamadmahgoub910 <mohamadmahgoub1@Outlook.com>
2025-10-04 14:13:09 +0300

    Binding Razor FE for Product BE

--- EC01/Controllers/HomeController.cs
+++ EC01/Controllers/HomeController.cs
@@ -1,12 +1,15 @@
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using EC01.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EC01.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        // باخد اوبجكت من الابلكيشن دي بي كونتكست عشان هستخدمه ف اكتر من اكسن بكتبه فوق كده مش مع الاكشن
+        private ApplicationDBContext _context = new();
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -15,7 +18,9 @@ namespace EC01.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var products = _context.Products/*.AsNoTracking()*/.Include(e=>e.Category).AsQueryable();
+            // Add Filter 
+            return View(products.ToList());
         }
 
         public IActionResult Privacy()
