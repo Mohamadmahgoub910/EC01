d8fb3fe2722198fae8c70f5b483f7b6a5e17e253
Mohamadmahgoub910 <mohamadmahgoub1@Outlook.com>
2025-10-06 20:45:40 +0300

    Category/ Create/Edit

---
+++ EC01/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EC01.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        ApplicationDBContext _context = new ApplicationDBContext();
+        public IActionResult Index()
+        {
+            var categories = _context.Categories.AsNoTracking().AsQueryable();
+            return View(categories.AsEnumerable());
+        }
+
+        // Create
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Edi()
+        {
+            return View();
+        }
+        // Edit 
+        [HttpGet]
+        public IActionResult Create(int? id)
+        {
+            var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- EC01/Program.cs
+++ EC01/Program.cs
@@ -18,8 +18,9 @@ namespace EC01
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
             app.UseRouting();
 
             app.UseAuthorization();
@@ -31,6 +32,7 @@ namespace EC01
                 .WithStaticAssets();
 
             app.Run();
+
         }
     }
 }
