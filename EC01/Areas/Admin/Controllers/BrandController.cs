40336d25fdcfc67eeda7a3515f574db4a2773e43
Mohamadmahgoub910 <mohamadmahgoub1@Outlook.com>
2025-10-08 01:43:09 +0300

    Brand Crud (hack from Category)

---
+++ EC01/Areas/Admin/Controllers/BrandController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EC01.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BrandController : Controller
+    {
+        ApplicationDBContext _context = new ApplicationDBContext();
+        public IActionResult Index()
+        {
+            var brands = _context.Brands.AsNoTracking().AsQueryable();
+            return View(brands.AsEnumerable());
+        }
+
+        // Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Brand brand)
+        {
+            _context.Brands.Add(brand);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // Edit 
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var brand = _context.Brands.FirstOrDefault(c => c.Id == id);
+            if (brand is null)
+            {
+                return RedirectToAction("NotFoundPage", "Home");
+            }
+            return View(brand);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Brand brand)
+        {
+            _context.Brands.Update(brand);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        //Delete
+        public IActionResult Delete(int id)
+        {
+            var brand = _context.Brands.FirstOrDefault(c => c.Id == id);
+            if (brand is null)
+            {
+                return RedirectToAction("NotFoundPage", "Home");
+            }
+            _context.Brands.Remove(brand);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+    
+}
