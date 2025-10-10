b22d4b9f933fb78c5b6efd3c760e2a77f4afb1b8
Mohamadmahgoub910 <mohamadmahgoub1@Outlook.com>
2025-10-10 20:49:16 +0300

    Update All Brand Changes

--- EC01/Models/Category.cs
+++ EC01/Models/Category.cs
@@ -1,9 +1,15 @@
-﻿namespace EC01.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EC01.Models
 {
     public class Category
     {
         public int Id { get; set; }
+        //[Required]
+        //[MinLength(3)]
+        //[MaxLength(100)]
         public string Name { get; set; } = string.Empty;
+        //[MaxLength(500)]
         public string? Description { get; set; }
         public bool Status { get; set; }
 
